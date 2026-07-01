1bae2abeca20cf47e9f7588d11211f780610ace7
indhmharanii <husnulsyukur6@gmail.com>
2026-07-01 19:50:12 +0800

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
+
+function App() {
+  return (
+    <>
+      <h1>💻 Laptop Recommendation</h1>
+    </>
+  );
+}
+
+export default App;
